de5477f507c27a7bb3fa271ae490208bf8d815c7
zmonx <chumpitak@gmail.com>
2020-01-10 15:32:49 +0700

    add class

--- src/profit/Salary.java
+++ src/profit/Salary.java
@@ -1,5 +1,45 @@
 package profit;
 
 public class Salary {
+	
+	public double sarary;
+	public double commission;
+	
+	
+	
+	public Salary(double sarary, double scommission) {
+		super();
+		this.sarary = sarary;
+		this.commission = commission;
+	}
 
+
+
+	public double getSarary() {
+		return sarary;
+	}
+
+
+
+	public void setSarary(double sarary) {
+		this.sarary = sarary;
+	}
+
+
+
+	public double getCommission() {
+		return commission;
+	}
+
+
+
+	public void setCommission(double commission) {
+		this.commission = commission;
+	}
+	
+	
+	
+	
+	
+	
 }
