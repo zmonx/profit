b0bc8c5949b8c65c94af65bbc4d96fe2bd6161f3
paicrush <dcdc07411@gmail.com>
2020-01-10 16:22:46 +0700

    projectA setEmployees

--- src/profit/profitDemo.java
+++ src/profit/profitDemo.java
@@ -1,11 +1,29 @@
 package profit;
 
+import java.util.Scanner;
+
 public class profitDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello word ");
-		System.out.println("test 2");
+		Arraylist<Employees> em =new Arraylist<Employees>();
+		Arraylist<Salary> s =new Arraylist<Salary>();
+		Scanner input =new Scanner (System.in);
+		do {
+			System.out.println("Enter employeesID :");
+			String id=input.next();
+			System.out.println("Enter employees name :");
+			String name=input.next();
+			System.out.println("Enter salary");
+			double money =input.nextInt();
+			em.add(new Employees(name,id));
+			s.add(new Salary(sa));
+			System.out.println("Do you want enter again [y/n]");
+			char ch =input.next().charAt(0);
+			}while(ch=='y');
+		for(int i=0;i<em.size();i++) {
+			em[i].setSalary(s[i]);
+		}
+			
 	}
 
 }
