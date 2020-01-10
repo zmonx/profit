94b06e0bb3dc8397ae720e46f4ea3c07ecb0ff0b
wic0144 <Arm_ruttanard@hotmail.com>
2020-01-10 15:40:02 +0700

    hello word

--- src/profit/profitDemo.java
+++ src/profit/profitDemo.java
@@ -4,7 +4,8 @@ public class profitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello word");
+		System.out.println("hello word ");
+		System.out.println("test 2");
 	}
 
 }
