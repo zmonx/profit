2ef5859b33e3c3b43bc8c482fea77e4ddaa2624c
paicrush <dcdc07411@gmail.com>
2020-01-10 18:17:38 +0700

    finish projectA

--- src/profit/profitDemo.java
+++ src/profit/profitDemo.java
@@ -1,19 +1,5 @@
 <<<<<<< HEAD
 package profit;
-
-public class profitDemo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello word ");
-		System.out.println("test 2");
-		System.out.println("test 3");
-	}
-
-}
-=======
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
